a5a5ceb53840868c200c7e41ae2e0cec350d4765
FLopesT <89878572+FLopesT@users.noreply.github.com>
2022-02-16 17:01:08 -0300

    1.0

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 //const bodyParser = require("body-parser");
 const storeRoute = require("./routes/store");
